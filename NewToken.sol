d87c39125a92a442fd5e512aab936a31ca3747b7
Uron51 Alfa Centaury <89109968+Feli3ro1990@users.noreply.github.com>
2023-06-29 23:44:42 -0500

    Update NewToken.sol

--- NewToken.sol
+++ NewToken.sol
@@ -4,10 +4,10 @@ import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	constructor() public {
-		totalSupply = 123;
-		name = "Best ever coin";
-		decimals = 4;
-		symbol = "BEC";
+		totalSupply = 27000000;
+		name = "Siglos";
+		decimals = 9;
+		symbol = "SGL";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
