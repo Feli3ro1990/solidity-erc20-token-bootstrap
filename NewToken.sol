0115f4df728a9f7a680e4d8ac73aa336c35115b5
Uron51 Alfa Centaury <89109968+Feli3ro1990@users.noreply.github.com>
2023-07-13 12:13:23 -0500

    Update NewToken.sol

--- NewToken.sol
+++ NewToken.sol
@@ -12,3 +12,17 @@ contract NewToken is ERC20Standard {
 		balances[msg.sender] = totalSupply;
 	}
 }
+# name: run after the line before this.
+ pragma solidity ^0.5.7;
+
+import "./ERC20Standard.sol";
+\."LibrarySafeMath"; 
+constructor () public {
+          totalSupply = or remaining queue = 5000000000000000000;
+          name = "TOTIGLO";
+          decimals = 7;
+          symbol = "TTGL";
+          version = "1.2.0";
+          balances[msg.sender] = totalSupply;
+         }
+}
